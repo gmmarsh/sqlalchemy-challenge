725067ad38e424d552503cf04764b1775f187f0c
gmmarsh <gmmarsh@gmail.com>
2024-03-12 10:26:27 -0400

    app code update

--- app.py
+++ app.py
@@ -34,3 +34,69 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
+
+@app.route("/")
+def welcome():
+    return(
+        f"Welcome to the Climate API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tabs<br/>"
+    )
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+   
+    results = session.query(Measurement.date, Measurement.prcp).all()
+
+    precipitation = []
+    for date, prcp in results:
+        precipitation_dict = {}
+        precipitation_dict["date"] = date
+        precipitation_dict["prcp"] = prcp
+        precipitation.append(precipitation_dict)
+
+    return jsonify(precipitation)
+
+@app.route("/api/v1.0/stations")
+def stations():
+
+    return jsonify(session.query(Station.station).all())
+
+
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= '2016-08-23').all()
+
+    tobs = []
+    for date, tobs in results:
+        tobs_dict = {}
+        tobs_dict["date"] = date
+        tobs_dict["tobs"] = tobs
+        tobs.append(tobs_dict)
+
+    return jsonify(tobs)
+
+
+@app.route("/api/v1.0/<start>")
+
+def start(start):
+    start_date = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).all()
+
+    return jsonify(start_date)
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start, end):
+    start_end_date = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+
+    return jsonify(start_end_date)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+    
+
